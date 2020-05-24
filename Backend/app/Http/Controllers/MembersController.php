00c8bd953aa6079b9766b2f000800433cdf6eafe
hengkysanjaya <hengkysanjaya204@gmail.com>
2020-05-24 23:17:58 +0700

    add new ERD & members API

---
+++ Backend/app/Http/Controllers/MembersController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Members;
+use http\Env\Request;
+
+
+class MembersController extends BaseController
+{
+    public function index()
+    {
+//        $members = DB::table('members')->get();
+        return $this->sendResponse(Members::all(), 'Members retrieved successfully');
+    }
+
+    public function show($id)
+    {
+        $member = Members::find($id);
+        return $this->sendResponse($member, 'Data retrieved successfully');
+    }
+
+    public function store(Request $request)
+    {
+        $member = Members::create($request->all());
+
+        return $this->sendResponse($member, 'Data added successfully');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $member = Members::findorFail($id);
+        $member->update($request->all());
+        return $this->sendResponse($member, 'Data updated successfully');
+    }
+}
---
+++ Backend/app/Members.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Members extends Model
+{
+    protected $fillable = [
+        'fullname', 'email', 'password'
+    ];
+}
