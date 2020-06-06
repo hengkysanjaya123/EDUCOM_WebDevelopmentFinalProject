1be0a2136bb505f5fc2346a7244ea5867332724a
hengkysanjaya <hengkysanjaya204@gmail.com>
2020-06-06 12:02:29 +0700

    login validation

--- Frontend/eduka-interface/src/app/login/login.component.ts
+++ Frontend/eduka-interface/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth/auth.service';
 import {HTTPCustomResponse} from '../../models/response.model';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -15,7 +16,7 @@ export class LoginComponent implements OnInit {
   response: HTTPCustomResponse = null;
   errorMsg = '';
 
-  constructor(private api: AuthService) {
+  constructor(private api: AuthService, private router: Router) {
   }
 
   ngOnInit(): void {
@@ -23,15 +24,28 @@ export class LoginComponent implements OnInit {
 
   doLogin() {
     this.response = null;
+    this.errorMsg = '';
 
     const data = {
       email: this.email,
       password: this.password
     };
 
+    if (!data.email || !data.password) {
+      this.errorMsg = 'Email and Password must be filled';
+      return;
+    }
+
     this.api.doAuthentication(data)
       .subscribe(res => {
         this.response = res;
+
+        if(!res.success){
+          this.errorMsg = res.message;
+          return;
+        }
+
+        this.router.navigate(['/room']);
       }, err => {
         console.log('error:' + err.json);
         this.response = null;
