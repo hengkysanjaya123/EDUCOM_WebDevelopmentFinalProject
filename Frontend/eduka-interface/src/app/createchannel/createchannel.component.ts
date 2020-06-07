f7f879832bc24ef9983d47df338fa17b0bece3e2
Ryan Rusli <ryanrusli1407@yahoo.com>
2020-06-07 18:57:14 +0700

    ketinggalan

--- Frontend/eduka-interface/src/app/createchannel/createchannel.component.ts
+++ Frontend/eduka-interface/src/app/createchannel/createchannel.component.ts
@@ -9,9 +9,16 @@ export class CreatechannelComponent implements OnInit {
 
   constructor() { }
   channelName: string;
+  loading = false;
+  errorMsg = '';
   ngOnInit(): void {
   }
   createChannel() {
-  	
+	this.errorMsg = '';
+
+    if (!this.channelName) {
+      this.errorMsg = 'Please enter the Room Code';
+      return;
+    }
   }
 }
