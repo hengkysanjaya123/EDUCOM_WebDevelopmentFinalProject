cc66f959f0de300bd198908428727f66ef00746b
Ryan Rusli <ryanrusli1407@yahoo.com>
2020-06-07 19:31:47 +0700

    create channel functionality finished, channel services

--- Frontend/eduka-interface/src/app/createchannel/createchannel.component.ts
+++ Frontend/eduka-interface/src/app/createchannel/createchannel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChannelService } from '../../services/channel/channel.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-createchannel',
@@ -7,18 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatechannelComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api: ChannelService, private router: Router,) { }
   channelName: string;
+  visibility: string;
   loading = false;
   errorMsg = '';
   ngOnInit(): void {
   }
+
+  //TODO: Change room id to 
   createChannel() {
 	this.errorMsg = '';
 
-    if (!this.channelName) {
-      this.errorMsg = 'Please enter the Room Code';
+    if (!this.channelName || ! this.visibility) {
+      this.errorMsg = 'Please fill in all the fields';
       return;
     }
+    const data = {
+      room_id: 1,
+      channel_name: this.channelName, 
+      visibility: this.visibility	
+    };
+	this.loading = true;
+	    this.api.createChannel(data).subscribe(res => {
+	      this.loading = false;
+	      console.log('success: ' + JSON.stringify(res));
+	      if (!res.success) {
+	        this.errorMsg = res.message;
+	        return;
+	      }
+
+	      this.router.navigate(['\classrooms', {message: 'Channel created!'}]);
+	    }, err => {
+	      this.loading = false;
+	      console.log('error: ' + JSON.stringify(err));
+    });
   }
 }
---
+++ Frontend/eduka-interface/src/services/channel/channel.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {HTTPCustomResponse} from '../../models/response.model';
+import {tap} from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    }
+  )
+};
+const apiUrl = 'http://localhost:8000/api';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChannelService {
+
+  constructor(private http: HttpClient) { 
+  }
+
+  createChannel(data): Observable<HTTPCustomResponse> {
+    return this.http.post<HTTPCustomResponse>(apiUrl + '/room_channels', data, httpOptions)
+      .pipe(
+        tap((response: HTTPCustomResponse) => console.log('result: ' + JSON.stringify(response)))
+      );
+  }}
