62cf14d68b7776b795af38c43dd926fa30e22334
Ryan Rusli <ryanrusli1407@yahoo.com>
2020-06-19 19:35:54 +0700

    cors fix attempt 1

--- Frontend/eduka-interface/src/services/channel/channel.service.ts
+++ Frontend/eduka-interface/src/services/channel/channel.service.ts
@@ -11,7 +11,7 @@ const httpOptions = {
     }
   )
 };
-const apiUrl = 'https://call.educom.club/api';
+const apiUrl ='https://cors-anywhere.herokuapp.com/https://call.educom.club/api';
 
 @Injectable({
   providedIn: 'root'
